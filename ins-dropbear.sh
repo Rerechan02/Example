215e2206c43d3b968a28210fb7b66671dc293ed6
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-06-03 23:46:09 +0700

    ins-dropbear.sh

--- ins-dropbear.sh
+++ ins-dropbear.sh
@@ -5,6 +5,6 @@ sleep 0.5
 apt-get -y --purge remove dropbear
 sleep 0.5
 apt -y install dropbear
-wget -O /etc/default/dropbear https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/dropbear
+wget -O /etc/default/dropbear https://raw.githubusercontent.com/Rerechan02/Example/main/fix/dropbear
 sleep 0.5
 /etc/init.d/dropbear restart
