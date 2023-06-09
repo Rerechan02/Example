fdc4a181ed700745a2a0d0b4ed556266d3cbe470
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-06-09 19:46:09 +0700

    insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -7,7 +7,7 @@ cd
 #wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/Rerechan02/Example/main/sshws/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Rerechan02/Example/main/sshws/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Rerechan02/Example/main/sshws/ws-stunnel
-wget -O /usr/local/bin/cf https://raw.githubusercontent.com/Rerechan02/Example/main/cf.sh
+wget -O /usr/local/bin/cf https://raw.githubusercontent.com/Rerechan02/XRAY/main/cf.sh
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
